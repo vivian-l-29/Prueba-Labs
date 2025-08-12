7e813b64b261090ddbc0d79c9ad793b53c60c632
byviusk <valopez@scl.edu.gt>
2025-08-12 10:39:11 -0600

    Uso del termino ocultar

--- componentes/banner/banner.js
+++ componentes/banner/banner.js
@@ -7,8 +7,13 @@ export function banner() {
     div.appendChild(span);
 
     let aBoton = document.createElement("a");
-    aBoton.className = "banner-boton"; 
-    aBoton.href = "/pages/login/login.html";
+    aBoton.className = "banner-boton";
+
+    // Construye basada en la ubicación actual
+    const path = window.location.pathname;
+    const base = path.substring(0, path.lastIndexOf('/')); // Carpeta actual
+    aBoton.href = `${base}/pages/login/login.html`;
+
     aBoton.textContent = "SHOP NOW";
     div.appendChild(aBoton);
 
